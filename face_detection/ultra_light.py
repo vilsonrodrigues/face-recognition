7fea97e778a50f038024816feb95f6c1593864d1
vilson <vilsonrodrigues07@hotmail.com>
2023-08-20 00:19:56 -0300

    fix docstring, model not support batch inference

--- face_detection/ultra_light.py
+++ face_detection/ultra_light.py
@@ -6,7 +6,7 @@ class UltraLight(ONNXRuntimeModel):
     This model is a lightweight face detection model
     designed for edge computing devices.
 
-    Input tensor is `(N x 3 x height x width)` with mean
+    Input tensor is `(1 x 3 x height x width)` with mean
     values `(127, 127, 127)` and scale factor `1.0 / 128.`
 
     Input image have to be previously converted to
@@ -14,8 +14,8 @@ class UltraLight(ONNXRuntimeModel):
     version-RFB-320 model
     (or 640 x 480 for version-RFB-640 model).
 
-    The model outputs two arrays `(N x 4420 x 2)` and
-    `(N x 4420 x 4)` of scores and boxes.
+    The model outputs two arrays `(2 x 4420 x 2)` and
+    `(4 x 4420 x 4)` of scores and boxes.
     """
 
     def __init__(self, MODEL_PATH: str):
