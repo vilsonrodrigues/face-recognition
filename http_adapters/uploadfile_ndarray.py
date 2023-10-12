8c249aeebd1556dcc9d74b6c728f8c6b070157a7
vilson <vilsonrodrigues07@hotmail.com>
2023-10-12 16:39:58 -0300

    add uploadfile adapter to load a image. Add cors support

---
+++ http_adapters/uploadfile_ndarray.py
@@ -0,0 +1,55 @@
+from io import BytesIO
+
+import numpy as np
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from ray import serve
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@serve.deployment(
+    name='UploadFile',
+    ray_actor_options={'num_gpus': 0.0,
+                       'num_cpus': 0.05,
+                       'memory': 100,
+                       'runtime_env':
+                            {'image': 'rayproject/ray:2.7.0-py310',
+                            #"run_options": ["--cap-drop SYS_ADMIN","--log-level=debug"]
+                             }
+                       },
+    health_check_period_s=10,
+    health_check_timeout_s=30,
+    autoscaling_config={'min_replicas': 1,
+                        'max_replicas': 16,
+                        'initial_replicas': 4,},
+)
+@serve.ingress(app)
+class UploadFileToNdarray:
+
+    @app.post('/')
+    async def upload_image(file: UploadFile = File(description='Image with faces',
+                                                          media_type='image/*')):
+
+        image_bytes = await image.read()
+
+        try:
+
+            image = Image.open(BytesIO(image_bytes))
+
+            image_np = np.array(image)
+
+            return image_np
+
+        except:
+
+            raise HTTPException(status_code=400, detail='Invalid image format')
