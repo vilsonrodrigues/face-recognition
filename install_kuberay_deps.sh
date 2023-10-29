1f60b995a6abd211400a91f0789d1a3cc73ade93
vilson <vilsonrodrigues07@hotmail.com>
2023-10-29 14:53:32 -0300

    remove old svcs and add expose port in shell script

--- install_kuberay_deps.sh
+++ install_kuberay_deps.sh
@@ -2,13 +2,6 @@
 
 # Install Kuberay, Prometheus and Grafana #
 
-# add Kuberay Helm
-helm repo add kuberay https://ray-project.github.io/kuberay-helm/
-helm repo update
-
-# Install both CRDs and KubeRay operator v1.0.0-rc.0.
-helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0-rc.0
-
 # Install Prometheus and Grafana
 git clone https://github.com/ray-project/kuberay.git
 ./kuberay/install/prometheus/install.sh
@@ -16,9 +9,20 @@ git clone https://github.com/ray-project/kuberay.git
 # Remove kuberay folder
 rm -r ./kuberay
 
+# add Kuberay Helm
+helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+helm repo update
+
+# Install both CRDs and KubeRay operator v1.0.0-rc.0.
+helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0-rc.0
+
 # Apply Clusters-IP
-kubectl apply -f kubernetes/prometheus-svc.yaml
-kubectl apply -f kubernetes/grafana-svc.yaml
-kubectl apply -f kubernetes/ray-dashboard-svc.yaml
+kubectl expose pod $(kubectl get pod -l ray.io/node-type=head | awk 'NR>1 {print $1}') --type=ClusterIP --name=kuberay-metrics-clusterip --port=8080
+
+kubectl expose service prometheus-prometheus-kube-prometheus-prometheus-0 -n prometheus-system --type=ClusterIP --name=prometheus-clusterip --port=9090
+
+kubectl expose deployment prometheus-grafana -n prometheus-system --type=ClusterIP --name=grafana-clusterip --port=3000
+
+kubectl expose service $(kubectl get pod -l ray.io/node-type=head | awk 'NR>1 {print $1}') --type=ClusterIP --name=ray-dashboard-clusterip --port=8265
 
 echo "Complete installation"
