d6438447fc1d58d5396fe779c3afbdf2d1017ad0
vilson <vilsonrodrigues07@hotmail.com>
2023-12-10 21:02:38 -0300

    refactor check img channels to return bool results

--- processing/utils.py
+++ processing/utils.py
@@ -5,20 +5,41 @@ import numpy as np
 from PIL import Image
 
 
-def check_img_channels(img: np.ndarray) -> np.ndarray:
-    """This function verify image dimension to
-    to ensure you have correct color channel
+def check_img_channels(image: np.ndarray) -> bool:
+    """
+    This function checks the channels of a given image.
+
+    The function performs the following checks:
+    0. Checks if there are three color channels.
+    1. Checks if the first channel is greater than 0.
+    2. Checks if the second channel is greater than 0.
+    3. Checks if the third channel is greater than 0.
+
+    If any of these checks fail, the function returns False. Otherwise, it returns True.
+
     Args:
-        img
+        image: The input image in numpy format.
 
     Returns:
-        frame: Frame with corrects channels
+        True if all checks pass, False otherwise.
     """
-    # If image 2D, add 1D to switch to 3D
-    if len(img.shape) == 2:
-        return np.reshape(img, img.shape + (1,))
-    else:
-        return img
+    # Check if there are three color channels
+    if image.ndim != 3 or image.shape[2] != 3:
+        return False
+
+    # Check if the first channel is greater than 0
+    if np.max(image[:, :, 0]) <= 0:
+        return False
+
+    # Check if the second channel is greater than 0
+    if np.max(image[:, :, 1]) <= 0:
+        return False
+
+    # Check if the third channel is greater than 0
+    if np.max(image[:, :, 2]) <= 0:
+        return False
+
+    return True
 
 
 def convert_bytes_to_base64(img_bytes: bytes) -> str:
