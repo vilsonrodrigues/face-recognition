f3f8937318d9fca1a7a83bf0aff24ed5f8a8878e
vilson <vilsonrodrigues07@hotmail.com>
2023-12-13 16:37:49 -0300

    upgrade kuberay version to 1.0.0 and upgrade apiVersion to ray.io/v1

--- install_kuberay_deps.sh
+++ install_kuberay_deps.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Install Kuberay, Prometheus and Grafana #
-
-# Install Prometheus and Grafana
-git clone https://github.com/ray-project/kuberay.git
-./kuberay/install/prometheus/install.sh
-
-# Remove kuberay folder
-rm -r ./kuberay
+# Install Kuberay #
 
 # add Kuberay Helm
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
@@ -15,5 +8,3 @@ helm repo update
 
 # Install both CRDs and KubeRay operator v1.0.0
 helm install kuberay-operator kuberay/kuberay-operator --version 1.0.0
-
-echo "Complete installation"
